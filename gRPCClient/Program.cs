3c5afa5625109075ab9e2523b838fe4402ce0f5b
alifuatyildirim <alifuat.yildirim@hotmail.com>
2022-08-01 11:31:49 +0300

    first commit

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcServiceClient;
+using System;
+
+namespace gRPCClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greetClient = new Greeter.GreeterClient(channel);
+            HelloReply response = greetClient.SayHello(new HelloRequest { Name = "Gençay" });
+            Console.WriteLine($"Gelen Cevap : {response.Message}");
+        }
+    }
+}
